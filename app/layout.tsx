fbaa00f1e8940cd3ae7a83d7847a75be3e23e73e
Franced3000 <franc.spata@gmail.com>
2024-07-13 09:58:47 +0200

    bug favicon fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,10 +24,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <Head>
-        <link rel="shortcut icon" href="/image/icon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/image/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/image/icon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/image/icon-16x16.png"/>
+        <link rel="shortcut icon" href="../public/image/icon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="../public/image/aple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="../public/image/icon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="../public/image/icon-16x16.png"/>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </Head>
